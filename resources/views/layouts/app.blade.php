c5c39d52e0d3f95778e7ad588a71e6f89cf55ac3
Moses <ajiremoses@gmail.com>
2020-06-03 21:26:56 +0100

    Added title to all view files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ config('app.name', 'SmartDiary') }} - @yield('title')</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -77,5 +74,8 @@
             <p>Thank you for using {{ config('app.name', 'SmartDiary') }} </p>
         </footer>
     </div>
+
+     <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>

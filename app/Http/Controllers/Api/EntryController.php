06d1b71b570aa47224086ca7bb1e68b5c4f10bd7
Moses <ajiremoses@gmail.com>
2020-06-04 13:11:07 +0100

    Updated EntryController for Api

---
+++ app/Http/Controllers/Api/EntryController.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Http\Requests\ApiEntryFormRequest;
+
+use App\User;
+
+use App\Entry;
+
+class EntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $entries = auth()->user()->entries;
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $entries
+            ], 200);
+    }
+
+  
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ApiEntryFormRequest $request)
+    {
+        $entry = new Entry;
+
+        $entry->body = $request->body;
+
+        if(auth()->user()->entries()->save($entry))
+        {
+            return response()->json([
+                    'success' => true,
+                    'message' => 'Entry created successfully'
+                ], 201);
+        }
+        else
+        {
+            return response()->json([
+                    'success' => false,
+                    'message' => 'Sorry, there was a problem creating your entry.'
+                ], 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $entry = auth()->user()->entries->find($id);
+
+        if($entry)
+        {
+            return response()->json(
+                [
+                    'success' => true,
+                    'data' => $entry->toArray()
+                ], 200);
+        }
+        else
+        {
+            return response()->json([
+                    'success' => false,
+                    'message' => 'Sorry, entry not found.'
+                ], 404);
+        }
+    }
+
+   
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ApiEntryFormRequest $request, $id)
+    {
+        $entry = auth()->user()->entries->find($id);
+
+        if(!$entry)
+        {
+            return response()->json([
+                    'success' => false,
+                    'message' => 'Sorry, entry not found.'
+                ], 404);
+        }
+        else 
+        {
+            $entry->body = is_null($request->body) ? $entry->body : $request->body;
+
+            if(auth()->user()->entries()->save($entry))
+            {
+                return response()->json([
+                        'success' => true,
+                        'message' => 'Entry updated successfully.'
+                    ], 200);
+            }
+            else
+            {
+                return response()->json([
+                        'success' => false,
+                        'message' => 'Sorry, there was a problem updating your response'
+                    ], 400);
+            }
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $entry = auth()->user()->entries->find($id);
+
+        if(!$entry)
+        {
+            return response()->json([
+                    'success' => false,
+                    'message' => 'Sorry, entry not found.'
+                ], 404);
+        }
+        else
+        {
+            if($entry->delete())
+            {
+                return response()->json([
+                        'success' => true,
+                        'message' => 'Entry deleted successfully'
+                    ], 204);
+            }
+            else
+            {
+                return response()->json([
+                        'success' => false,
+                        'message' => 'Sorry, there was a problem deleting your entry.'
+                    ], 400);
+            }
+        }
+    }
+}
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,5 +17,32 @@ class UserController extends Controller
     	$user->name = $request->name;
     	$user->email = $request->email;
     	$user->password = bcrypt($request->password);
+
+    	$token = $user->createToken('SmartDiary')->accessToken;
+
+    	return response()->json(['token' => $token], 200);
+    }
+
+    public function login(Request $request)
+    {
+    	$credentials = [
+    		'email' => $request->email,
+    		'password' => $request->password
+    	];
+
+    	if(auth()->attempt($credentials))
+    	{
+    		$token = auth()->user()->createToken('SmartDiary')->accessToken;
+    		return response()->json(['token' => $token], 200);
+    	} 
+    	else
+    	{
+    		return response()->json(['error' => 'Sorry, you are not authorized to perform this action.'], 401);
+    	}
+    }
+
+    public function details()
+    {
+    	return response()->json(['user' => auth()->user()], 200);
     }
 }
